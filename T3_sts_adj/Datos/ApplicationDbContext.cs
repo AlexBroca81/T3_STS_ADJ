ce06820030b04f8f55efe14f5ed86aac7202477e
AlexBroca81 <n00341365@upn.pe>
2024-06-25 06:53:20 +0200

    Commit sin autorizacion

---
+++ T3_sts_adj/Datos/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using T3_sts_adj.Models;
+
+namespace T3_sts_adj.Datos
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Libro> Libro { get; set; }
+    }
+}
---
+++ T3_sts_adj/Models/Libro.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace T3_sts_adj.Models
+{
+    public class Libro
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Tema { get; set; }
+
+        public string Editorial { get; set; }
+        [Range(1900, 3000, ErrorMessage = "El año de publicación debe estar entre 1900 y 3000.")]
+        public int AnioPublicacion { get; set; }
+        [Range(10, 1000, ErrorMessage = "El número de páginas debe estar entre 10 y 1000.")]
+        public int Paginas { get; set; }
+        public string Categoria { get; set; }
+
+    }
+}
